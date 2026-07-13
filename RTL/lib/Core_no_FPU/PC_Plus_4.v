fe11e5b78c11b5082ca02636e5dd36080da09a42
pedroctag <pedroctag@hotmail.com>
2026-07-13 15:00:47 -0300

    remocao de Core_no_FPU do versionamento

--- RTL/lib/Core_no_FPU/PC_Plus_4.v
+++
@@ -1,8 +0,0 @@
-module PC_Plus_4(
-		input wire [31:0]  PC,
-		output wire [31:0] PCPlus4 
-		);
-
-   assign PCPlus4 = PC + 32'd4;
-
-endmodule
--- RTL/lib/Core_no_FPU/Result_Mux.v
+++
@@ -1,9 +0,0 @@
-module Result_Mux (
-		   input wire [31:0]  ALUResult,ReadData,PC_Plus_4,
-		   input wire [31:0]  ResultSrc,
-		   output wire [31:0] Result
-		   );
-
-   assign Result = ResultSrc[1] ? PC_Plus_4 :(ResultSrc[0] ? ReadData : ALUResult);
-
-endmodule
