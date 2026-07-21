bb77c56a1143555a9de9f9e3855ae7bdb886e149
pedroctag <pedroctag@hotmail.com>
2026-07-21 16:16:01 -0300

    limpeza de arquivos

--- SYNTHESIS/base.cmd
+++
@@ -1,8 +0,0 @@
-# Cadence Genus(TM) Synthesis Solution, Version 22.17-s071_1, built Sep 12 2024 12:37:05
-
-# Date: Tue Jul 14 10:03:32 2026
-# Host: microeletronica-047 (x86_64 w/Linux 4.18.0-553.137.1.el8_10.x86_64) (4cores*8cpus*1physical cpu*Intel(R) Core(TM) i7-4790 CPU @ 3.60GHz 8192KB)
-# OS:   Rocky Linux release 8.10 (Green Obsidian)
-
-source script_genus.tcl
-exit
